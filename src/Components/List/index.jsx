cf3f79a3263f0709a2ae77ce24d73b02dfbbac68
Camilo-Suarez98 <camilo.suarez85@gmail.com>
2023-08-24 20:57:33 -0500

    Include parallel promises

--- src/Components/List/index.jsx
+++ src/Components/List/index.jsx
@@ -1,26 +1,14 @@
 import React, { useState, useEffect } from "react"
-import axios from "axios"
-import { getEpisodes } from "./util"
+import { getData } from "./util"
 
 const List = () => {
-  const [ episodes, setEpisodes ] = useState([])
+  const [episodes, setEpisodes] = useState([])
 
   useEffect(() => {
-    fetchEpisodes()
+    getData().then(data => {
+      setEpisodes(data)
+    })
   }, [])
-
-  const fetchEpisodes = async () => {
-    const episodePromise = getEpisodes()
-    const [ episodeData ] = await Promise.all([ episodePromise ])
-    setEpisodes(episodeData)
-  }
-
-  const fetchCharacters = async () => {
-    const fetchCharactersByEpisode = episodes.map(episode => (
-      episode.characters.slice(0, 10).map(character => character)
-    ))
-  }
-  fetchCharacters()
 
   return (
     <div>
@@ -29,7 +17,7 @@ const List = () => {
           episodes.map(episode => {
             return (
               <React.Fragment key={episode.id}>
-                <li><h2>{episode.name} - {episode.episode}</h2></li>
+                <li><h2>{episode.title} - {episode.episode}</h2></li>
                 <ul>
                   {episode.characters.slice(0, 10).map((character, index) => {
                     return (
--- src/Components/List/util.js
+++ src/Components/List/util.js
@@ -5,11 +5,38 @@ const getEpisodes = async () => {
   return data.results
 }
 
-// const getCharacters = async () => {
-//   const { data } = await axios.get(`${url}/character`)
-//   return data.results
-// }
+const getCharacter = async (url) => {
+  const { data } = await axios.get(url)
+  return data
+}
+
+const getData = async () => {
+  const episodes = getEpisodes()
+
+  const characters = episodes.reduce((acc, item) => {
+    return [...acc, ...item.characters.slice(0, 10)]
+  }, [])
+
+  const charactersPromise = characters.map(url => {
+    return getCharacter(url)
+  })
+
+  const parallelPromise = await Promise.all(charactersPromise)
+
+  const data = episodes.map((episode) => {
+    return {
+      id: episode.episode,
+      title: `${episode.name} - ${episode.episode}`,
+      dateToAir: episode.air_date,
+      characters: episode.characters.slice(0, 10).map((url) => {
+        return parallelPromise.find((item) => item.url === url)
+      })
+    }
+  })
+
+  return data;
+}
 
 export {
-  getEpisodes
+  getData
 }
